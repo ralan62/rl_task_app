56b74ebdfec3aac14a54a6910915b6a4f99196c2
ralan62 <ralflange@ralan.de>
2025-07-10 13:52:06 +0200

    solved task 4.5.2.2

--- lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_2.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_2.dart
@@ -4,6 +4,26 @@ class S4522 extends StatelessWidget {
   const S4522({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return MyStatelessWidget();
+  }
+}
+
+class MyStatelessWidget extends StatelessWidget {
+  const MyStatelessWidget({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            Text("Hello Ralf"),
+            ElevatedButton(
+              onPressed: (){}, 
+              child: const Text('Klick mich')
+            ),
+          ]
+        )
+      )
+    );
   }
 }
